1a0067979b650afb7584f8ef303934850ff893ee
shijianhong <shijianhong@shijianhongdemacbook-pro.local>
2016-05-19 16:32:49 +0800

    222

--- lllll/lllll/ViewController.m
+++ lllll/lllll/ViewController.m
@@ -16,6 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
+    NSLog(@"henhao");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
